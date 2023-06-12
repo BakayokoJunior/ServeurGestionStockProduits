42427f2c6b6aa61c42ec2ff69261794ef938ab5d
BakayokoJunior <bakayokobassindoujunior0150@gmail.com>
2023-06-12 16:37:04 +0200

    First commit

---
+++ src/main/java/com/BakDev/ServeurGestionStockProduits/ServeurGestionStockProduitsApplication.java
@@ -0,0 +1,20 @@
+package com.BakDev.ServeurGestionStockProduits;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProduitsApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+	System.out.println("Hello world");
+		
+	}
+
+}
